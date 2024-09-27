ea52cc62d78cc2ca36c8484476ebf200b3388949
Titus Wormer <tituswormer@gmail.com>
2024-09-27 11:43:28 +0200

    Refactor to simplify flow

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-import process from 'node:process'
-import chalk from 'chalk'
 import {npmTools} from './lib/index.js'
-import * as config from './config/index.js'
 
-npmTools.run(
-  {
-    // Note: ghToken needs `admin:org` and `repo` scopes.
-    ghToken: process.env.GH_TOKEN || process.env.GITHUB_TOKEN,
-    // Note: npmToken must be granted by an owner of all orgs.
-    npmToken: process.env.NPM_TOKEN,
-    ...config
-  },
-  done
-)
-
-/**
- * @param {unknown} error
- * @returns {undefined}
- */
-function done(error) {
-  if (error) {
-    console.log(chalk.red('✖') + ' error')
-    console.error(error)
-  } else {
-    console.log(chalk.green('✓') + ' done')
-  }
-}
+npmTools()
